515ef2cf0b5ba19526c2a88ee087a1c692dfee1d
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2025-03-14 16:04:36 +0530

    add framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,45 @@ import FloodImpactPrediction from "./components/FloodImpactPrediction";
 import Services from "./components/Services";
 import AboutUs from "./components/AboutUs";
 import Contact from "./components/Contact";
+import { motion } from "framer-motion";
 
 function App() {
+  const gridContainerVariants = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { staggerChildren: 0.25 } },
+  };
+
+  const gridSquareVariants = { hidden: { opacity: 0 }, show: { opacity: 1 } };
   return (
     <>
-      <Navbar />
-      <Hero />
-      <RealTimeFloodAnalysis />
-      <FloodImpactPrediction />
-      <Services />
-      <AboutUs />
-      <Contact />
+      <motion.section
+        variants={gridContainerVariants}
+        initial="hidden"
+        animate="show"
+      >
+        <motion.div variants={gridSquareVariants}>
+          <Navbar />
+        </motion.div>
+        <motion.div variants={gridSquareVariants}>
+          <Hero />
+        </motion.div>
+        <motion.div variants={gridSquareVariants}>
+          <RealTimeFloodAnalysis />
+        </motion.div>
+        <motion.div variants={gridSquareVariants}>
+          <FloodImpactPrediction />
+        </motion.div>
+
+        <motion.div variants={gridSquareVariants}>
+          <Services />
+        </motion.div>
+        <motion.div variants={gridSquareVariants}>
+          <AboutUs />
+        </motion.div>
+        <motion.div variants={gridSquareVariants}>
+          <Contact />
+        </motion.div>
+      </motion.section>
     </>
   );
 }
