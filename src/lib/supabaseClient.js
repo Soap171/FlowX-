9e401f3d88ef378cc80706052acaf2949730cf94
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2025-03-17 13:46:58 +0530

    add env variables

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bpzidglllurtwqddzjfy.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwemlkZ2xsbHVydHdxZGR6amZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNDA0OTAsImV4cCI6MjA1NzYxNjQ5MH0.vATMyAc948eHkQtXd5KKjBZED04M6qWtzxXSJLz3G60";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URI;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
